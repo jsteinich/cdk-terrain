747c23146d58bb2c11318324f589702e9ec5eac2
Jon Steinich <jsteinich@gmail.com>
2026-03-29 15:32:24 -0500

    Revert "temp add info for debugging test failures"
    This reverts commit 4aee7fc2884913a1ab3863bc3ba631bd9aeab956.

--- packages/@cdktn/cli-core/src/lib/models/deploy-machine.ts
+++ packages/@cdktn/cli-core/src/lib/models/deploy-machine.ts
@@ -172,7 +172,6 @@ export function handleLineReceived(send: (event: DeployEvent) => void) {
     }
 
     if (!hideOutput) {
-      console.log(`Unhidden output: ${output}`);
       send({
         type: "OUTPUT_RECEIVED",
         output,
